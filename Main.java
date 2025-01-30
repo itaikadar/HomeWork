72578fb3be503fd14465dbefc0d64dec2c61a56a
itaikadar <itai.kadar@gmail.com>
2025-01-30 17:31:22 +0200

    Homework

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //1  Check Voting Eligibility
+
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        double age = sc.nextDouble();
+        if (age >= 18) {
+            System.out.println("You are eligible to vote");
+        } else if (age <= 18) {
+          System.out.println("You are not eligible to vote");
+        }*/
+
+
+        //2 Simple Calculator
+
+        /*Scanner sc = new Scanner(System.in);
+         System.out.println("PLease choose two numbers and the action you want to do with them options are +,-,*,/");
+        int number1 = sc.nextInt();
+        String action = sc.next();
+        int number2 = sc.nextInt();
+         if (action.equals("+")){
+        System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));}
+         else if (action.equals("-")){
+          System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));}
+         else if (action.equals("*")){
+         System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));}
+        else if (action.equals("/")){
+        System.out.println(number1 + " / " + number2 + " = " + (number1 / number2));}*/
+
+
+        //3
+
+
+
+
+    }
+}

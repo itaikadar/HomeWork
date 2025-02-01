d44397f032debb8c01ccbb2bb474fe432fc2c4ee
itaikadar <itai.kadar@gmail.com>
2025-02-01 18:16:27 +0200

    this is the homework

--- Main.java
+++ Main.java
@@ -3,37 +3,149 @@ public class Main {
     public static void main(String[] args) {
         //1  Check Voting Eligibility
 
-        /*Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your age");
-        double age = sc.nextDouble();
-        if (age >= 18) {
-            System.out.println("You are eligible to vote");
-        } else if (age <= 18) {
-          System.out.println("You are not eligible to vote");
-        }*/
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter your age");
+//        double age = sc.nextDouble();
+//        if (age >= 18) {
+//            System.out.println("You are eligible to vote");
+//        } else if (age <= 18) {
+//          System.out.println("You are not eligible to vote");
+//        }
 
 
         //2 Simple Calculator
 
-        /*Scanner sc = new Scanner(System.in);
-         System.out.println("PLease choose two numbers and the action you want to do with them options are +,-,*,/");
-        int number1 = sc.nextInt();
-        String action = sc.next();
-        int number2 = sc.nextInt();
-         if (action.equals("+")){
-        System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));}
-         else if (action.equals("-")){
-          System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));}
-         else if (action.equals("*")){
-         System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));}
-        else if (action.equals("/")){
-        System.out.println(number1 + " / " + number2 + " = " + (number1 / number2));}*/
+//        Scanner sc = new Scanner(System.in);
+//         System.out.println("PLease choose two numbers and the action you want to do with them options are +,-,*,/");
+//        int number1 = sc.nextInt();
+//        String action = sc.next();
+//        int number2 = sc.nextInt();
+//         if (action.equals("+")){
+//        System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));}
+//         else if (action.equals("-")){
+//          System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));}
+//         else if (action.equals("*")){
+//         System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));}
+//        else if (action.equals("/")){
+//        System.out.println(number1 + " / " + number2 + " = " + (number1 / number2));}
 
 
-        //3
+//        //3
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please choose a number");
+//        int number = sc.nextInt();
+//        if (number % 5 == 0 && number % 3 == 0) {
+//            System.out.println("The number is divisible by 5 and 3");
+//        } else if (number % 3 == 0) {
+//            System.out.println("The number is only divisible by 3");
+//        } else if (number % 5 == 0) {
+//            System.out.println("The number is only divisible by 5");
+//        }
 
 
+        //4
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter your password");
+//        String password = sc.nextLine();
+//
+//        if (password.equals("Zxasqw12")) {
+//            System.out.println("This password is predefined");
+//        } else {
+//            System.out.println("Your password isn't predefined, please try again");
+//        }
 
 
+        //5
+
+//       Scanner sc = new Scanner(System.in);
+//       System.out.println("please choose 3 numbers");
+//       double number1 = sc.nextDouble();
+//       double number2 = sc.nextDouble();
+//       double number3 = sc.nextDouble();
+//
+//        if (number1 > number2 && number1 > number3) {
+//            System.out.println(number1 + " is the largest number");}
+//            else if (number2 > number3 && number2 > number3) {
+//                System.out.println(number2 + " is the largest number");}
+//                else if (number3 > number2 && number3 > number2) {
+//                    System.out.println(number3 + " is the largest number");}
+
+        //7
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("please choose 3 numbers");
+//    double number1=sc.nextDouble();
+//    double number2=sc.nextDouble();
+//    double number3=sc.nextDouble();
+//    System.out.println("The average of those numbers is ");
+//    System.out.println(number1+number2+number3 /3);
+
+
+//        //8
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Input an integer:");
+//        int num = sc.nextInt();
+//
+//        int sum = 0;
+//        while (num > 0) {
+//            sum += num % 10;
+//            num /= 10;
+//        }
+//
+//        System.out.println("The sum of the digits is: " + sum);
+
+
+//        //9
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter one single character");
+//        String letter = sc.nextLine();
+//        if (letter.equals("a") || letter.equals("e") || letter.equals("i") || letter.equals("o") || letter.equals("u")) {
+//            System.out.println("its an vowel");
+//
+//        } else {
+//            System.out.println("its not an vowel");
+//        }
+//    }
+
+//
+//        //10
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter your grade");
+//        int grade = sc.nextInt();
+//        if (grade >= 90) {
+//            System.out.println("Your grade is A");
+//        }else if (grade >= 80) {
+//            System.out.println("Your grade is B");
+//        }else if (grade >= 70) {
+//            System.out.println("Your grade is C");
+//        }else {
+//            System.out.println("Your grade is F");
+//        }
+
+        //11
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter 2 numbers");
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        if (a > 0 && b > 0) {
+//            System.out.println("both numbers are positive");
+//        } else if (a < 0 && b < 0) {
+//            System.out.println("both numbers are negative");
+//        }
+//        if (a == 0 && b == 0) {
+//            System.out.println("both numbers are zero");
+//        } else if (a > 0 && b < 0) {
+//            System.out.println(a + " is positive and " + b + " is negative");
+//        } else if (a < 0 && b > 0) {
+//            System.out.println(a + " is negative and " + b + " is positive");
+//        }
     }
 }
+
+
+
+
+
+
+
+
+
